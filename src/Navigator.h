34d81b98c2812ad5e5e8202ebdec6b448644c4ad
Yosef Silberhaft <yossisilb@gmail.com>
2016-12-29 05:09:58 -0600

    added and removed variables

--- src/Navigator.h
+++ src/Navigator.h
@@ -7,14 +7,14 @@
 #ifndef NAVIGATOR_H_
 #define NAVIGATOR_H_
 
-
 #include <geometry_msgs/PoseStamped.h>
-#include <ros/ros.h>
 #include <nav_msgs/GetMap.h>
+#include <ros/ros.h>
 #include <vector>
 #include <fstream>
 #include <stdlib.h>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -27,16 +27,15 @@ public:
     bool requestMap(ros:: NodeHandle &nh);
     void readMap(const nav_msgs::OccupancyGrid& map);
     void printGridToFile();
-
-
+	Location starting_location;
+	Location goal_location;
 
 private:
+	double mapResolution;
 	double robot_size;
-	// Grid map definition
 	int rows;
 	int cols;
-	double mapResolution;
-	vector<vector<bool> > grid;
+	vector<vector<bool>> grid;
 };
 
 
