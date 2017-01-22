6acc3272a8b2f23c64eca9f9bafd7f1f50b6d7a8
Yosef Silberhaft <yossisilb@gmail.com>
2017-01-22 10:30:01 -0600

    Added new functions

--- src/Navigator.h
+++ src/Navigator.h
@@ -6,7 +6,6 @@
 
 #ifndef NAVIGATOR_H_
 #define NAVIGATOR_H_
-
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/GetMap.h>
 #include <ros/ros.h>
@@ -19,7 +18,7 @@
 using namespace std;
 
 //Define locations as x,y
-typedef pair<int, int> Location;
+typedef pair<double, double> Location;
 
 struct Node {
     int x, y;
@@ -42,9 +41,22 @@ public:
     void SetNewGridSize(int rows, int cols);
     double idiv_ceil(double numerator, double denominator);
     void CreateReducedGrid(int k_rows, int k_cols);
-    bool CheckSubMatrix(int x_start, int y_start, int x_end, int y_end);
-	Location starting_location;
-	Location goal_location;
+    int CheckSubMatrix(int x_start, int y_start, int x_end, int y_end);
+    Location CalculateRealPosition(double x, double y, double resolution);
+    void CreateAlgoGrid(vector<vector<int> > grid, int rows, int cols);
+    void CreateVectorForAlgorithm();
+    pair<int, int> center_coordinated;
+    vector<int> one_dim_grid;
+    int rows;
+    int cols;
+    // Pairs of the given start and goal location
+    Location given_start;
+	Location given_goal;
+    Location center_grid;
+    Location big_start_location;
+    Location big_goal_location;
+    Location s_start_location;
+    Location s_goal_location;
 
 private:
 	double mapResolution;
@@ -52,14 +64,11 @@ private:
 	double pixels_size;
 	double size_map_units;
     int perimeter;
-    int rows;
-	int cols;
     int n_rows;
     int n_cols;
 	vector<vector<int> > grid;
 	vector<vector<int> > int_grid;
     vector<vector<int> > robot_size_grid;
-	vector<int> single_vector;
-
+    vector<vector<int> > algo_grid;
 };
 #endif
